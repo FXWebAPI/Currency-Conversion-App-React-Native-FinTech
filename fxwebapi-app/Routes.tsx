cc888913df1fa62cd38adfc19825f6d432d97875
justas <samuolis.just@gmail.com>
2019-11-15 18:45:03 +0000

    Create initial dashboard screen / Make named imports from screens possible

--- fxwebapi-app/Routes.tsx
+++ fxwebapi-app/Routes.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import LoginScreen from './screens/LoginScreen';
+import { LoginScreen, DashboardScreen } from './screens';
 
 const AppNavigator = createStackNavigator({
+  Dashboard: DashboardScreen,
   Login: LoginScreen,
 }, {
   defaultNavigationOptions: { header: null, headerForceInset: { top: 'never', bottom: 'never' } }
---
+++ fxwebapi-app/screens/DashboardScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function DashboardScreen() {
+
+  return (
+    <View style={styles.container}>
+      <Text>ASDF</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+});
