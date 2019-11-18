84058755e82b8ba2a9802b9503a15239dc771cbe
justas <samuolis.just@gmail.com>
2019-11-18 02:58:36 +0000

    Added bottom tab navigation (need update icons)

--- fxwebapi-app/Routes.tsx
+++ fxwebapi-app/Routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 import { LoginScreen, DashboardScreen } from './screens';
 
 const AppNavigator = createStackNavigator({
@@ -10,4 +12,47 @@ const AppNavigator = createStackNavigator({
   defaultNavigationOptions: { header: null, headerForceInset: { top: 'never', bottom: 'never' } }
 });
 
-export default createAppContainer(AppNavigator);
+const TabNavigator = createBottomTabNavigator({
+  Dashboard: {
+    screen: DashboardScreen,
+    path: '/dashboard',
+    navigationOptions: {
+      tabBarIcon: ({focused, tintColor}) => {
+        return <MaterialIcons name='home' size={24} color={tintColor} />
+      },
+    }
+  },
+  Spot: {
+    screen: LoginScreen,
+    path: '/dashboard',
+    navigationOptions: {
+      tabBarIcon: ({focused, tintColor}) => {
+        return <MaterialIcons name='tonality' size={24} color={tintColor} />
+      },
+    }
+  },
+  Order: {
+    screen: DashboardScreen,
+    path: '/dashboard',
+    navigationOptions: {
+      tabBarIcon: ({focused, tintColor}) => {
+        return <MaterialIcons name='swap-horiz' size={24} color={tintColor} />
+      },
+    }
+  },
+  Deal: {
+    screen: DashboardScreen,
+    path: '/dashboard',
+    navigationOptions: {
+      tabBarIcon: ({focused, tintColor}) => {
+        return <MaterialIcons name='receipt' size={24} color={tintColor} />
+      },
+    }
+  }
+}, {
+  tabBarOptions: {
+    showLabel: false
+  }
+});
+
+export default createAppContainer(TabNavigator);
